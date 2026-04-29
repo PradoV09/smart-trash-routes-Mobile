fde799556adfb5fa933c67e454e81520f4850e75
JonatanCueroMoreno <cuero.loco313@.gmail.com>
2026-04-29 14:49:41 -0500

    Actualización completa del proyecto

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { 
+  IonHeader, IonToolbar, IonTitle, IonContent, 
+  IonProgressBar, IonGrid, IonRow, IonCol, 
+  IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle,
+  IonButton, IonLabel, AlertController
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  standalone: true,
+  imports: [
+    IonHeader, IonToolbar, IonTitle, IonContent,
+    IonProgressBar, IonGrid, IonRow, IonCol,
+    IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle,
+    IonButton, IonLabel
+  ],
 })
 export class Tab3Page {
-  constructor() {}
+  constructor(private alertCtrl: AlertController) {}
+
+  async finalizarTurno() {
+    const alert = await this.alertCtrl.create({
+      header: 'Confirmar',
+      message: '¿Estás seguro de que deseas finalizar tu turno?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Finalizar',
+          handler: () => {
+            console.log('Turno finalizado');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
 }
